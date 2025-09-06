17ade91f4a8d3178a69bfcae4ea4f0ca078bbc09
Hammuel15 <yepdiehammuel15@gmail.com>
2025-09-06 14:10:10 +0100

    set global variabmles and dom elements

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+// Global variables
+let departureWeatherData = null;
+let arrivalWeatherData = null;
+
+// DOM Elements
+const searchForm = document.getElementById('searchForm');
+const loadingSection = document.getElementById('loadingSection');
+const errorSection = document.getElementById('errorSection');
+const resultsSection = document.getElementById('resultsSection');
 
